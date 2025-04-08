c78b6d66d61bb58285a5378ac5dff56fc071259f
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-08 12:11:09 -0500

    set up clerk for auth and handle sign in functionality in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
+
 import Link from "next/link";
+import { SignedIn, SignedOut, useUser, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 const Navbar = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (isLoaded) {
+    <p>Loading...</p>;
+  }
   return (
     <nav>
       <div>
-      <Link href="/">What to eat</Link>
+        <Link href="/">The Menu</Link>
       </div>
       <div>
-       
+        <SignedIn>
+          <Link href="/mealplan">Mealplan</Link>
+          {user?.imageUrl ? (
+            <Link href="/profile">
+              <Image
+                src={user.imageUrl}
+                alt="profile picture"
+                width={40}
+                height={40}
+              />
+            </Link>
+          ) : (
+            <div></div>
+          )}
+          <SignOutButton></SignOutButton>
+        </SignedIn>
+        <SignedOut>
+          <button>Sign out</button>
+        </SignedOut>
       </div>
     </nav>
   );
