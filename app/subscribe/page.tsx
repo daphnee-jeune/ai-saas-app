e08ef2b1076cbbb26efeba117da5d265e5be8598
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-09 15:25:12 -0500

    verbiage change

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -62,7 +62,7 @@ const Subscribe = () => {
                   : "bg-emerald-100 text-emerald-700  hover:bg-emerald-200 "
               }  mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium disabled:bg-gray-400 disabled:cursor-not-allowed`}
             >
-              Subscribe {plan.name}
+              Purchase {plan.name}
             </button>
           </div>
         ))}
