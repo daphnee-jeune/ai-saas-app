aa588664005d9b99e80bdf6db5267a4acb6e9faf
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-09 15:24:40 -0500

    Create and style subscribe page

---
+++ lib/plans.ts
@@ -0,0 +1,51 @@
+export interface Plan {
+  name: string;
+  amount: number;
+  currency: string;
+  interval: string;
+  isPopular?: boolean;
+  description: string;
+  features: string[];
+}
+
+export const availablePlans: Plan[] = [
+  {
+    name: "Weekly plan",
+    amount: 9.99,
+    currency: "USD",
+    interval: "week",
+    description: "Great if you want to try it out before committing.",
+    features: [
+      "Unlimited AI meal plans",
+      "AI nutrition insigihts",
+      "Cancel anytime",
+    ],
+  },
+  {
+    name: "Monthly plan",
+    amount: 19.99,
+    currency: "USD",
+    interval: "month",
+    isPopular: true,
+    description:
+      "Perfect for on-going meal planning suited to your needs to reach your goals.",
+    features: [
+      "Unlimited AI meal plans",
+      "Priority AI support",
+      "Cancel anytime",
+    ],
+  },
+  {
+    name: "Weekly plan",
+    amount: 299.99,
+    currency: "USD",
+    interval: "year",
+    description:
+      "Best value for those committed to improving their diet long-term.",
+    features: [
+      "Unlimited ai meal plans",
+      "All premium features",
+      "Cancel anytime",
+    ],
+  },
+];
